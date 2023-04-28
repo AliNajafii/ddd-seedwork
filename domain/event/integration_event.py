bcbecdd788dc9d3c2cf053e58528ff133e67cdb8
Ali Najafi <aliosys.nj@gmail.com>
2023-04-28 20:28:26 +0430

    domain and integration events created

---
+++ domain/event/integration_event.py
@@ -0,0 +1,18 @@
+from .event import Event
+
+class IntegrationEvent(Event):
+    """
+    An integration event.
+    .. note::
+        This class should never be instantiated by the user.
+        Subclass and implement
+        :meth:`~ddd.domain.event.event.Event.get_serialized_payload`.
+    """
+    def __init__(self, name, version="1", date=None, sender=None, corr_ids=None):
+        super(IntegrationEvent, self).__init__(
+            name=name,
+            version=version,
+            date=date,
+            sender=sender,
+            corr_ids=corr_ids,
+        )
